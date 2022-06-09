a74de4bd9e03c72702c7f59f1f538404055604f8
Franz Gatt <franz.gatt@student.uibk.ac.at>
2022-06-09 15:53:50 +0200

    gpx-Dateien in das overlay eingebunden

--- main.js
+++ main.js
@@ -74,4 +74,6 @@ L.control.fullscreen().addTo(map);
 overlays.gpx.addTo(map);
 
 // GPX Track Layer implementieren
-let gpxTrack = new L.GPX("../data/23.gpx")
+let gpxTrack = new L.GPX("../data/23.gpx", {
+    async: true,
+}).addTo(overlays.gpx);
