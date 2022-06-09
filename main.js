b89b99b6d4ab6eeb61719b8a2836855415337384
Franz Gatt <franz.gatt@student.uibk.ac.at>
2022-06-09 16:04:18 +0200

    Linie strichliert und schwarz gemacht

--- main.js
+++ main.js
@@ -83,4 +83,8 @@ let gpxTrack = new L.GPX("../data/23.gpx", {
     iconSize: [32, 37],
     iconAnchor: [16, 37],
     },
+    polyline_options: {
+        color: "black",
+        dashArray: [2, 5],
+    }
 }).addTo(overlays.gpx);
