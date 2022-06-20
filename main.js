34ed12331ef54b3a5bcba00c1258b2d2018ba21f
Franz Gatt <franz.gatt@student.uibk.ac.at>
2022-06-20 15:18:01 +0200

    ./data-Fehler ausgebessert

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ L.control.fullscreen().addTo(map);
 overlays.gpx.addTo(map);
 
 // GPX Track Layer implementieren
-let gpxTrack = new L.GPX("../.data/23.gpx", {
+let gpxTrack = new L.GPX("./data/23.gpx", {
     async: true,
     marker_options: {
         startIconUrl: 'icons/start.png',
